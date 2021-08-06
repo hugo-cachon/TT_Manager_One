02cc3cddae6c67a666e72d791fdfa32643c6dcff
Hugo Cachon <hugo.cachon@hetic.net>
2021-08-06 15:50:59 +0200

    Update Tasks.php

--- Backend/Models/Tasks.php
+++ Backend/Models/Tasks.php
@@ -30,52 +30,54 @@
 
         public function getTaskById()
         {
-            $stmt = "SELECT id, title, description, status, FROM " . $this->table . " WHERE id = ? LIMIT 0,1";
+            $stmt = "SELECT id, user_id, title, description, date FROM " . $this->table . " WHERE id =:id ";
     
             $query = $this->connexion->prepare($stmt);
             
-            $query->bindParam(1, $this->id);
+            $query->bindParam(":id", $this->id);
             
             $query->execute();
     
             $row = $query->fetch(PDO::FETCH_ASSOC);
     
             $this->id = $row['id'];
+            $this->user_id = $row['user_id'];
             $this->title = $row['title'];
             $this->description = $row['description'];
-            $this->status = $row['status'];
+            $this->creation_date = $row['date'];
         
         }
 
         public function createTask()
         {
-            $stmt = "INSERT INTO " . $this->table . " (id, user_id, title, status, description)
-            VALUES (:id, :user_id, :title, :status, :description)";
+            $stmt = "INSERT INTO " . $this->table . " (id, user_id, title, description, date)
+            VALUES (:id, :user_id, :title, :description, :date)";
     
             $query = $this->connexion->prepare($stmt);
     
             $query->bindParam(":id", $this->id);
             $query->bindParam(":user_id", $this->user_id);
             $query->bindParam(":title", $this->title);
-            $query->bindParam(":status", $this->status);
             $query->bindParam(":description", $this->description);
+            $query->bindParam(":date", date("Y-m-d"));
     
             if($query->execute())
             {
                 return true;
             }
             return false;
-    
         }
 
         public function updateTask()
         {
-            $stmt = "UPDATE " . $this->table . " SET title=:title, description=:description";
+            $stmt = "UPDATE " . $this->table . " SET title=:title, description=:description WHERE id=:id ";
     
             $query = $this->connexion->prepare($stmt);
     
             $query->bindParam(":title", $this->title);
             $query->bindParam(":description", $this->description);
+            $query->bindParam(":id", $this->id);
+            
     
             if($query->execute())
             {
