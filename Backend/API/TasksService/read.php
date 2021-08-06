21e28da2108c3b3aa62670271816c533df1eeaa0
Hugo Cachon <hugo.cachon@hetic.net>
2021-08-06 15:46:20 +0200

    Update read.php

--- Backend/API/TasksService/read.php
+++ Backend/API/TasksService/read.php
@@ -1,47 +1,51 @@
 <?php 
 
 header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 if($_SERVER["REQUEST_METHOD"] == 'GET'){
 
     include_once '../../Config/Database.php';
     include_once '../../Models/Tasks.php';
-
-    $database = new db_connect();
+    
+    $database = new Db_connect();
     $db = $database->setConnection();
 
-    $tasks = new Tasks($db);
-    
-    $stmt = $tasks->getAllTasks();
+    $task = new Tasks($db);
 
-    if($stmt->rowCount() > 0)
+    $data = json_decode(file_get_contents("php://input"));
+
+    if(!empty($data->id))
     {
-        $tasks_array = [];
-        $tasks_array["tasks"] = [];
+        $task->id = $data->id;
 
-        while($row = $stmt->fetch(PDO::FETCH_ASSOC))
-        {
-            extract($row);
+        $task->getTaskById();
 
-            $data = [
-                "id" => $id,
-                "user_id" => $user_id,
-                "title" => $title,
-                "description" => $description
+        if($task->id != null){
+
+            $task_array = [
+                "id" => $task->id,
+                "user_id" => $task->user_id,
+                "title" => $task->title,
+                "description" => $task->description,
+                "creation_date" => $task->creation_date,
             ];
 
-            $tasks_array["tasks"][] = $data;
-        }
-
-        http_response_code(200);
-        print_r(json_encode($tasks_array));
+            http_response_code(200);
+            echo json_encode($task_array);
+    }
+    else
+    {
+        http_response_code(404);     
+        echo json_encode(["Task does not exists"]);
+    }
     }
 }
-else 
+else
 {
     http_response_code(405);
-    echo "Method Not Allowed";
-    die();
+    echo json_encode(["Method Not Allowed"]);
 }
-?>
