24d30a6547d6013a1d936aef524571a024cd1076
Hugo Cachon <hugo.cachon@hetic.net>
2021-08-06 15:44:55 +0200

    Update create.php

--- Backend/API/TasksService/create.php
+++ Backend/API/TasksService/create.php
@@ -6,33 +6,33 @@ header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
+
 if($_SERVER["REQUEST_METHOD"] == 'POST'){
 
     include_once '../../Config/Database.php';
     include_once '../../Models/Tasks.php';
 
-    $database = new Db_connect();
+    $database = new db_connect();
     $db = $database->setConnection();
 
-    $task = new Tasks($db);
+    $users = new Tasks($db);
 
     $data = json_decode(file_get_contents("php://input"));
 
-    if(!empty($data->user_id) && !empty($data->title) && 
-        !empty($data->description) && !empty($data->status))
+    if(!empty($data->description) && !empty($data->title))
     {
-        $task->user_id = $data->user_id;
-        $task->title = $data->title;
-        $task->description = $data->description;
-        $task->status = $data->status;
-
-        if($task->createTask())
+        $users->id = $data->id;
+        $users->title = $data->title;
+        $users->description = $data->description;
+        $users->user_id = $data->user_id;
+        
+        if($users->createTask())
         {
             http_response_code(201);
             echo json_encode(["Task has been added to the database"]);
         }
         else
-        {
+        { 
             http_response_code(503);
             echo json_encode(["Error"]);
         }
