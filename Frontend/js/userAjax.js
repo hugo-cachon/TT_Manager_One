8a0c30f884faf6cd880c1e323d1c47185c8e1aef
Hugo Cachon <hugo.cachon@hetic.net>
2021-08-06 15:52:10 +0200

    Update userAjax.js

--- Frontend/js/userAjax.js
+++ Frontend/js/userAjax.js
@@ -1,8 +1,9 @@
 const createUser = "http://localhost/TT_Manager_One/Backend/API/UserService/create.php";
 const getUsersUrl = "http://localhost/TT_Manager_One/Backend/API/UserService/read.php";
+const deleteUserUrl = "http://localhost/TT_Manager_One/Backend/API/UserService/delete.php";
 
 // create user
-$('#createUser').on("submit", function(e) {
+$('#createUser').on("submit", function() {
     let data = {
     "name": $("#username").val(),
     "email": $("#email").val()
@@ -18,19 +19,41 @@ $.ajax({
     error: function(err) {
         console.log(err);
     }
-});
+    });
 });
 
 
-// get users
+// read users 
+$.getJSON( getUsersUrl, function(data) {
+    var items = [];
+    $.each( data.users, function() {
+        $.each(this, function( key, val) {
+            items.push( "<li id='" + key + "'>" + val + "</li>" );      
+        })
+    });  
+    /*
+    $( "<ul/>", {
+      "class": "user-list",
+      html: items.join( "" )
+    }).appendTo( "body" );*/
+});
+
+
+/* Postman -> ok
+var userdelete = {
+    "id": "11"
+}
+
 $.ajax({
-    url: getUsersUrl,        
-    type: 'GET',
-    success : function(data, status){
-        console.log("DATA: " + data + "\nSTATUS: " + status);
-            
-        var responseData = data;
-        var responseStatus = status;
+    url: deleteUserUrl, 
+    data: JSON.stringify(userdelete),
+    method : 'DELETE',
+    processData: false,
+    success: function(data) {
+        console.log(data);
+    },
+    error: function(err) {
+        console.log(err)
     }
-})
-
+});
+*/
